0c3eb968e67756c80f2acec38b3fa0f226fb719a
dongshangxian <dantesx2012@gmail.com>
2016-12-15 11:52:43 +0800

    初始化项目

---
+++ SXSpeechRecognitionTwoWays/SXSpeechRecognitionTwoWays/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SXSpeechRecognitionTwoWays
+//
+//  Created by dongshangxian on 2016/12/15.
+//  Copyright © 2016年 Sankuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
