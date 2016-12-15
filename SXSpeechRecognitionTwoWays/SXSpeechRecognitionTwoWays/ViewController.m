97a5c2f6256f83d46c9ec09afeb9a341a8a75978
dongshangxian <dantesx2012@gmail.com>
2016-12-15 14:47:19 +0800

    增加权限plist配置，和bufferRequest的核心代码

--- SXSpeechRecognitionTwoWays/SXSpeechRecognitionTwoWays/ViewController.m
+++ SXSpeechRecognitionTwoWays/SXSpeechRecognitionTwoWays/ViewController.m
@@ -7,10 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <Speech/Speech.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *showBufferText;
 
+
+
+@property(nonatomic,strong)SFSpeechRecognizer *bufferRec;
+@property(nonatomic,strong)SFSpeechAudioBufferRecognitionRequest *bufferRequest;
+@property(nonatomic,strong)SFSpeechRecognitionTask *bufferTask;
+@property(nonatomic,strong)AVAudioEngine *bufferEngine;
+@property(nonatomic,strong)AVAudioInputNode *buffeInputNode;
 
 @end
 
@@ -19,6 +27,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
+
+    }];
 }
 
 
@@ -26,7 +39,50 @@
 }
 
 - (IBAction)startBufferR:(id)sender {
+    
+    self.bufferRec = [[SFSpeechRecognizer alloc]initWithLocale:[NSLocale localeWithLocaleIdentifier:@"zh_CN"]];
+    self.bufferEngine = [[AVAudioEngine alloc]init];
+    
+    if (_bufferTask != nil) {
+        [_bufferTask cancel];
+        _bufferTask = nil;
+    }
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryRecord error:nil];
+    [audioSession setMode:AVAudioSessionModeMeasurement error:nil];
+    [audioSession setActive:true error:nil];
+    
+    self.bufferRequest = [[SFSpeechAudioBufferRecognitionRequest alloc]init];
+    AVAudioInputNode *inputNode = [self.bufferEngine inputNode];
+    self.buffeInputNode = inputNode;
+    SFSpeechAudioBufferRecognitionRequest *tempRecognitionRequest = self.bufferRequest;
+    
+    tempRecognitionRequest.shouldReportPartialResults = true;
+    
+    self.bufferTask = [self.bufferRec recognitionTaskWithRequest:tempRecognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
+        
+        if (result != nil) {
+            self.showBufferText.text = result.bestTranscription.formattedString;
+        }
+        
+        if (error != nil) {
+            NSLog(@"%@",error.userInfo);
+        }
+    }];
+    
+    AVAudioFormat *format =[inputNode outputFormatForBus:0];
+    [inputNode installTapOnBus:0 bufferSize:1024 format:format block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
+        [self.bufferRequest appendAudioPCMBuffer:buffer];
+    }];
+    
+    [self.bufferEngine prepare];
+    
+    [self.bufferEngine startAndReturnError:nil];
+    
+    self.showBufferText.text = @"等待命令中.....";
 }
+
 - (IBAction)stopBufferR:(id)sender {
 }
 
